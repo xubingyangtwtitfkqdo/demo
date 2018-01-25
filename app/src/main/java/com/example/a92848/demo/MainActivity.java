7e4f6c454d46ddabbe8a4e4a9455ef2f56ec5275
unknown <92848@DESKTOP-I4QTRH5.(none)>
2018-01-25 17:08:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a92848/demo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a92848.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //人生啊
+    }
+}
